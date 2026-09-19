668b2151dcdd2a06741f15941d3c092b0c738f89
dacastro42 <108814849+dacastro42@users.noreply.github.com>
2026-09-18 19:54:49 -0500

    Initial commit

---
+++ src/views/MainEjemploHerencia.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package views;
+
+import models.Alquiler;
+
+/**
+ *
+ * @author pc
+ */
+public class MainEjemploHerencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Alquiler[] arregloAlquileres=new Alquiler[2];
+        
+        
+        
+    }
+    
+}
